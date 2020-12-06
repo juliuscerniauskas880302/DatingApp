d51df4883282ca8204c2aee44543b78c63105587
Julius Cerniauskas <julius.cerniauskas.mif@gmail.com>
2020-12-06 18:08:40 +0200

    End of section 6

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,5 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {AccountService} from '../services/account.service';
+import {Router} from '@angular/router';
+import {ToastrService} from 'ngx-toastr';
 
 @Component({
   selector: 'app-nav',
@@ -9,7 +11,7 @@ import {AccountService} from '../services/account.service';
 export class NavComponent implements OnInit {
   model: any = {};
 
-  constructor(public accountService: AccountService) {
+  constructor(public accountService: AccountService, private router: Router, private toastr: ToastrService) {
   }
 
   ngOnInit(): void {
@@ -17,11 +19,17 @@ export class NavComponent implements OnInit {
   }
 
   login(): void {
-    this.accountService.login(this.model).subscribe();
+    this.accountService.login(this.model).subscribe(response => {
+      this.router.navigateByUrl('/members');
+    }, error => {
+      console.log(error);
+      this.toastr.error(error.error);
+    });
   }
 
   logout(): void {
     this.accountService.logout();
+    this.router.navigateByUrl('/');
   }
 
 }
--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,5 +1,6 @@
 import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import {AccountService} from '../services/account.service';
+import {ToastrService} from 'ngx-toastr';
 
 @Component({
   selector: 'app-register',
@@ -10,7 +11,7 @@ export class RegisterComponent implements OnInit {
   @Output() cancelRegister = new EventEmitter();
   model: any = {};
 
-  constructor(private accountService: AccountService) {
+  constructor(private accountService: AccountService, private toastr: ToastrService) {
   }
 
   ngOnInit(): void {
@@ -20,7 +21,9 @@ export class RegisterComponent implements OnInit {
     this.accountService.register(this.model).subscribe(response => {
       console.log(response);
       this.cancel();
-    }, error => console.log(error));
+    }, error => {
+      this.toastr.error(error.error);
+    });
   }
 
   cancel(): void {
