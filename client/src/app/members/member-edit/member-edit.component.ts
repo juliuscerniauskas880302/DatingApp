ad249bceded168b9adf4fffceb90cace2e5216de
Julius Cerniauskas <julius.cerniauskas.mif@gmail.com>
2021-01-17 16:35:24 +0200

    added missing files

---
+++ client/src/app/members/member-edit/member-edit.component.ts
@@ -0,0 +1,49 @@
+import {Component, HostListener, OnInit, ViewChild} from '@angular/core';
+import {Member} from '../../models/member';
+import {User} from '../../models/user';
+import {AccountService} from '../../services/account.service';
+import {MembersService} from '../../services/members.service';
+import {take} from 'rxjs/operators';
+import {ToastrService} from 'ngx-toastr';
+import {NgForm} from '@angular/forms';
+
+@Component({
+  selector: 'app-member-edit',
+  templateUrl: './member-edit.component.html',
+  styleUrls: ['./member-edit.component.css']
+})
+export class MemberEditComponent implements OnInit {
+  @ViewChild('editForm') editForm: NgForm;
+  member: Member;
+  user: User;
+
+  @HostListener('window:beforeunload', ['$event']) unloadNotification($event: any) {
+    if (this.editForm.dirty) {
+      $event.returnValue = true;
+    }
+  }
+
+  constructor(private accountService: AccountService, private memberService: MembersService, private toastr: ToastrService) {
+    this.accountService.currentUser$
+      .pipe(take(1))
+      .subscribe(user => this.user = user);
+  }
+
+  ngOnInit(): void {
+    this.loadMember();
+  }
+
+  loadMember(): void {
+    this.memberService
+      .getMember(this.user.username)
+      .subscribe(member => this.member = member);
+  }
+
+  updateMember(): void {
+    this.memberService.updateMember(this.member).subscribe(() => {
+      this.toastr.success('Profile updated');
+      this.editForm.reset(this.member);
+    });
+  }
+
+}
